d47890010f8aa5b1ab65c3648da9ebc93a13e3ab
ANMSilva <nipunmadumal18@gmail.com>
2025-01-25 09:14:59 +0530

    folder structure changed

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        User user = new User("Nipun", "Madumal", "test@gmail.com");
-
-        System.out.println(user.toString());
-    }
-}
